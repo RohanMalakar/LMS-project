92ea741a63296f45bc79110e2da340efc7cf97a8
RohanMalakar <rohanmalakar5091@gmail.com>
2025-02-27 03:41:43 +0530

    changed origin in frontend

--- FRONTEND/src/Hellers/axiosinstance.js
+++ FRONTEND/src/Hellers/axiosinstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL="https://codescorer.onrender.com/api/v1"
+const BASE_URL="https://code-scorer.onrender.com/api/v1"
 
 const axiosInstance=axios.create();
 
