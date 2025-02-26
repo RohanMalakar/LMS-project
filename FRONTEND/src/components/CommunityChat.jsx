37e010ad085ddd9553584dcedfe4930207ad641e
RohanMalakar <rohanmalakar5091@gmail.com>
2025-02-27 03:43:01 +0530

    changed

--- FRONTEND/src/components/CommunityChat.jsx
+++ FRONTEND/src/components/CommunityChat.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import io from "socket.io-client";
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect("https://code-scorer.onrender.com");
 import { MdAttachFile } from "react-icons/md";
 import EmojiPicker from 'emoji-picker-react';
 import { GrEmoji } from "react-icons/gr";
