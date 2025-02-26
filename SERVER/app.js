eb3acd7d5c809969f101bc22125d1cc76db0934b
RohanMalakar <rohanmalakar5091@gmail.com>
2025-02-27 02:08:53 +0530

    change in code to remove extra port

--- SERVER/app.js
+++ SERVER/app.js
@@ -2,69 +2,72 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import userRoute from "./routers/user.route.js"
+import http from "http";
+import { Server } from "socket.io";
+
+import userRoute from "./routers/user.route.js";
 import errorMiddleware from "./middlewares/error.middlewares.js";
 import courseRoute from "./routers/course.route.js";
 import paymentRoute from "./routers/payment.routs.js";
 import mescellaniousRoute from "./routers/miscellaneous.js";
 import quizRouter from "./routers/quiz.route.js";
-import { Server } from "socket.io";
-import http from "http";
 import emailRouter from "./routers/email.route.js";
-import {createChat} from "./controllers/chat.controller.js";
 import chatRouter from "./routers/chat.route.js";
+import { createChat } from "./controllers/chat.controller.js";
 
-const app=express();
+const app = express();
 const server = http.createServer(app);
+
+// Middleware
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-// app.use(cors({
-//     origin: [process.env.FRONTEND_URL],
-//     credentials:true
-// }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-app.use(express.urlencoded({extended:true,limit:"16kb"}));
+// CORS Configuration
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
 
-app.use("/api/v1/data/",mescellaniousRoute);
-app.use("/api/v1/user/",userRoute);
-app.use("/api/v1/payment/",paymentRoute);
-app.use("/api/v1/courses/",courseRoute);
-app.use("/api/v1/quizzes/",quizRouter);
-app.use("/api/v1/email/",emailRouter);
-app.use("/api/v1/chat/",chatRouter);
+// Routes
+app.use("/api/v1/data", mescellaniousRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/payment", paymentRoute);
+app.use("/api/v1/courses", courseRoute);
+app.use("/api/v1/quizzes", quizRouter);
+app.use("/api/v1/email", emailRouter);
+app.use("/api/v1/chat", chatRouter);
 
-app.use("/",(req,res)=>{
-  res.send("Hey I am rohan malakar")   
-});
-
-
-app.all("*",(req,res,next)=>{
-      res.status(404)
-      res.send("OOPS! page not found")   
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Hey, I am Rohan Malakar");
 });
 
-app.use(errorMiddleware)
+// 404 Handler
+app.all("*", (req, res) => {
+  res.status(404).send("OOPS! Page not found");
+});
 
+// Error Middleware
+app.use(errorMiddleware);
+
+// Socket.io Setup
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+  cors: corsOptions,
 });
 
 io.on("connection", (socket) => {
-  console.log(`a user connected ${socket.id}`);
+  console.log(`A user connected: ${socket.id}`);
 
   socket.on("send_message", async (data) => {
     try {
-      const response=await createChat(data);
-      if(response.success===false){
+      const response = await createChat(data);
+      if (!response.success) {
         socket.emit("message_error", { message: "Failed to send message" });
         return;
       }
@@ -74,12 +77,17 @@ io.on("connection", (socket) => {
       socket.emit("message_error", { message: "Failed to send message" });
     }
   });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
 });
 
-server.listen(4000, () => {
-  console.log("listening on *:4000");
+// Start Server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
 
 export default app;
-export {io};
+export { io };
